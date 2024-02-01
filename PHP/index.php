b4cbde812a6e279b7db48b4d71f588b5fb65ef63
Giulius94 <giuliuscb@protonmail.com>
2024-02-01 11:43:11 +0100

    ok

---
+++ PHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+<?php
+$var = 'mariolino';
+echo 'ciao';
+echo $var;
+
+?>
+
+<h2><?= 'corso php'?></h2>
+	
+</body>
+</html>
