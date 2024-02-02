2057b1248cafa6f5ddd74a975ff7bceff65888e3
Giulius94 <giuliuscb@protonmail.com>
2024-02-02 09:23:12 +0100

    ok

--- PHP_S1-L2/index.php
+++ PHP_S1-L2/index.php
@@ -88,33 +88,6 @@ session_start();
         ?>
     </div>
 
-
-
-    <?php
-
-    // session_start();
-    // if(isset($_SESSION['utenti'])) {
-    //     foreach($_SESSION['utenti'] as $key => $value) {
-    //         print ($key."<br>");
-    //         foreach($value as $k => $v) {
-    //             print ($v."<br>");
-    //         }
-    //     }
-    // } else {
-    //     print "non esiste un cazzo";
-    // }
-    
-
-
-
-    ?>
-
-
-
-
-
-
-
     <!-- bootstrap  -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
