76812d8229e3ba0d3c96d2dce84a9b5fdbde85f7
Giulius94 <giuliuscb@protonmail.com>
2024-02-28 15:45:25 +0100

    invio dati al db

---
+++ S5-L1/config.php
@@ -0,0 +1,29 @@
+<?php
+
+    $config = [
+        'mysql_host' => 'localhost',
+        'mysql_user' => 'root',
+        'mysql_password' => ''
+    ];
+
+    $mysqli = new mysqli(
+        $config['mysql_host'],
+        $config['mysql_user'],
+        $config['mysql_password']);
+    if($mysqli->connect_error) { die($mysqli->connect_error); } 
+
+    $sql = 'CREATE DATABASE IF NOT EXISTS user;';
+    if(!$mysqli->query($sql)) { die($mysqli->connect_error); } // Creo il mio DB
+ 
+    $sql= 'USE user;';
+    $mysqli ->query($sql);
+    // Creo la tabella
+    $sqltable = 'CREATE TABLE IF NOT EXISTS users ( 
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        firstname VARCHAR(255) NOT NULL, 
+        lastname VARCHAR(255) NOT NULL, 
+        city VARCHAR(255) NOT NULL
+    )';
+    if(!$mysqli->query($sqltable)) { die($mysqli->connect_error); }
+
+?>
---
+++ S5-L1/index.php
@@ -0,0 +1,35 @@
+<?php require_once 'config.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP S5-L1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Benvenuti</h1>
+    <form class="row g-3" method="POST" action="controller.php">
+  <div class="col-md-6">
+    <label for="inputnome" class="form-label">Nome</label>
+    <input type="text" class="form-control" id="inputnome" name="nome">
+  </div>
+  <div class="col-md-6">
+    <label for="inputcognome" class="form-label">Cognome</label>
+    <input type="text" class="form-control" id="inputcognome" name="cognome">
+  </div>
+  <div class="col-md-6">
+    <label for="inputCity" class="form-label">City</label>
+    <input type="text" class="form-control" id="inputCity" name="city">
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Invia dati al DB</button>
+  </div>
+</form>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
