5a3072b590796a7ad08b9494f0e9572208043f7a
Giulius94 <giuliuscb@protonmail.com>
2024-03-01 17:20:14 +0100

    ok

--- S5-L1/index_csv.php
+++ S5-L1/index_csv.php
@@ -11,7 +11,7 @@ if(!file_exists($dir)) {
 
 $utenti = [];
 
-$sql = 'SELECT * FROM users;';
+$sql = "SELECT * FROM users;";
 $datiUtenti =  $mysqli ->query($sql);
 /* var_dump($datiUtenti); */
 foreach($datiUtenti as $datiUtenti) {
@@ -30,7 +30,7 @@ if($resource) {
 $resource = fopen($dir.$file, 'r');
 while($data = fgetcsv($resource)) {
   var_dump($data);
- /*  $querySql = "SELECT * FROM userscsv WHERE id = $data "; */
+ /* $querySql = "SELECT * FROM userscsv WHERE id = $data ";  */
   $sql = "INSERT INTO userscsv (id, firstname, lastname, city) VALUES ('$data[0]','$data[1]','$data[2]','$data[3]')";
   $mysqli ->query($sql);
 
